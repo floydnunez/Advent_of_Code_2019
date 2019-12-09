5f130e3117231fd65156abf3fe4d9d5e54507481
JPNR <juan.pablo.nunnez.rojas@gmail.com>
2019-12-09 18:07:52 -0300

    day 9 was HARD

--- day_09/day_09b.py
+++ day_09/day_09b.py
@@ -149,9 +149,8 @@ def process(data, input):
 #data = [109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99]
 #data = [1102,34915192,34915192,7,4,7,99,0]
 #data = [104,1125899906842624,99]
-output = process(data, [1])
+output = process(data, [2])
 
 print(output)
 
-#wrong (203)
-#answer 3429606717
+#answer 33679
